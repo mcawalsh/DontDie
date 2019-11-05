68050c5b7ac990817ba23c7ec4e5dcb6b85f4476
Mick Walsh <mca.walsh@gmail.com>
2019-11-05 21:25:37 +0000

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GameManager : MonoBehaviour
+{
+	public Tilemap groundMap;
+	public Tilemap itemMap;
+
+	public Tile groundTile;
+	public Tile wallTile;
+
+	public GameObject player;
+    
+    void Start()
+	{
+		InitialisGrid();
+		InitialisePlayer();
+	}
+
+	private void InitialisePlayer()
+	{
+		Instantiate(player, new Vector3(0, 0, 0), Quaternion.identity);
+	}
+
+	private void InitialisGrid()
+	{
+		int gridWidth = 10;
+		int gridHeight = 10;
+
+		for (int x = 0; x < gridWidth; x++)
+		{
+			for (int y = 0; y < gridHeight; y++)
+			{
+				groundMap.SetTile(new Vector3Int(x - gridWidth / 2, y - gridHeight / 2, 0), groundTile);
+			}
+		}
+	}
+
+	void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public Camera camera;
+	public float speed;
+
+	// private Vector2 moveInput;
+	private Rigidbody2D rb;
+	private Vector2 moveVelocity;
+
+	public GameObject[] projectiles;
+
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void Update()
+	{
+		var moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		moveVelocity = moveInput.normalized * speed;
+	}
+
+	private void FireProjectile()
+	{
+		Instantiate(projectiles[0], transform.position, projectiles[0].transform.rotation);
+	}
+
+	// This is where you should put all your physics based adjustments
+	private void FixedUpdate()
+	{
+		rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+	}
+}
