73244d069b5174c7ae08aae3b67ad48ce31ec730
Mick Walsh <mca.walsh@gmail.com>
2019-11-25 02:11:29 +0000

    Add enemy and animations

---
+++ Assets/Scripts/Characters/Log.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Log : Enemy
+{
+	public Transform target;
+	public float chaseRadius;
+	public float attackRadius;
+	public Transform homePosition;
+	private Animator animator;
+
+    void Start()
+    {
+		target = GameObject.FindWithTag("Player").transform;
+
+		animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+		CheckDistance();
+    }
+
+	void CheckDistance()
+	{
+		float currentDistance = Vector3.Distance(target.position, transform.position);
+
+		if (currentDistance <= chaseRadius)
+		{
+			animator.SetBool("wakeUp", true);
+			transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+		} else
+		{
+			animator.SetBool("wakeUp", false);
+		}
+	}
+}
